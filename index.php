09ea890fe47f12e26a3ed86619b9eb3000f05643
Fatih Yenice <fatih.ydev@gmail.com>
2025-05-21 11:44:20 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>SnackExpress</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/remixicon@4.6.0/fonts/remixicon.min.css">
+    <link rel="stylesheet" href="/assets/style/global.css" />
+    <script src="/assets/js/app.js" defer></script>
+</head>
+
+<body>
+
+    <header>
+        <div class="logo"><i class="ri-service-bell-line"></i> SnackExpress</div>
+    </header>
+
+    <nav>
+        <div class="nav--cta_disposition">
+            <a href="">Grosse faim</a>
+            <a href="">Trouver un resto</a>
+            <a href="">Qui sommes nous ?</a>
+            <a href="">Contact</a>
+            <a href="" class="commander-button">Commander</a>
+        </div>
+    </nav>
+
+    <section id="big--home_section">
+        <div class="big--home_filtre">
+            <div class="big--home_textdisposition">
+                <h1>Bienvenue chez SnackExpress </h1>
+                <h2>Le goût rapide, frais et savoureux à chaque bouchée !</h2>
+                <button type="button">VOIR LES MENUS</button>
+            </div>
+        </div>
+    </section>
+
+    <section id="nos-restaurants_section">
+        dd
+    </section>
+</body>
+
+</html>
