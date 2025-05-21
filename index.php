7e4ab34c46752d4dc5d113a9243f47bad38f6d09
Fatih Yenice <fatih.ydev@gmail.com>
2025-05-21 14:21:00 +0200

    modif

--- index.php
+++ index.php
@@ -12,26 +12,34 @@
 
 <body>
 
-    <header>
-        <div class="logo"><i class="ri-service-bell-line"></i> SnackExpress</div>
-    </header>
 
-    <nav>
-        <div class="nav--cta_disposition">
-            <a href="">Grosse faim</a>
-            <a href="">Trouver un resto</a>
-            <a href="">Qui sommes nous ?</a>
-            <a href="">Contact</a>
-            <a href="" class="commander-button">Commander</a>
-        </div>
-    </nav>
+    <div class="jecommande"></div>
+
+    <div class="all-header">
+        <header>
+            <div class="logo"><i class="ri-service-bell-line"></i> SnackExpress</div>
+        </header>
+
+        <nav>
+            <div class="nav--cta_disposition">
+                <a href="">Grosse faim</a>
+                <a href="">Trouver un resto</a>
+                <a href="">Qui sommes nous ?</a>
+                <a href="">Contact</a>
+                <a href="" class="commander-button">Commander</a>
+            </div>
+            <div class="icon-burger">
+                <i class="ri-menu-line"></i>
+            </div>
+        </nav>
+    </div>
 
     <section id="big--home_section">
         <div class="big--home_filtre">
             <div class="big--home_textdisposition">
                 <h1>Bienvenue chez SnackExpress </h1>
                 <h2>Le goût rapide, frais et savoureux à chaque bouchée !</h2>
-                <button type="button">VOIR LES MENUS</button>
+                <button type="button">VOIR LES MENUS <i class="ri-file-list-3-line"></i></button>
             </div>
         </div>
     </section>
